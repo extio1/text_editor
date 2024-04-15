46da9b20a6ba40491ff0d9366fccb985995d314b
tatyanakrivonogova <tatyanakrivonogova@github.com>
2024-04-15 21:57:51 +0700

    UP-10 Removed GlyphParams

--- include/glyphs/glyph.h
+++ include/glyphs/glyph.h
@@ -13,8 +13,9 @@ class Glyph {
 public:
     using GlyphPtr = std::shared_ptr<Glyph>;
 
-    explicit Glyph(const GlyphParams& _params) : params(_params) {}
-    Glyph(int x, int y, int width, int height) : params({x, y, width, height}) {}
+    explicit Glyph(const int _x, const int _y, const int _width, const int _height) : 
+    x(_x), y(_y), width(_width), height(_height) {}
+    Glyph(int _x, int _y, int _width, int _height) : x(_x), y(_y), width(_width), height(_height) {}
     virtual ~Glyph() = default;
 
     virtual void Draw(Window*) = 0;
@@ -25,41 +26,65 @@ public:
     }
     void ClearGlyph(Window* w);
 
-    bool Intersects(const Point& p) { 
-        return params.Intersects(p); 
+    bool Intersects(const Point& p) const noexcept {
+        if (p.x >= x && p.x <= x + width) {
+            if (p.y >= y && p.y <= y + height) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool Intersects(const int _x, const int _y, const int _width, const int _height) const {
+        return Intersects({_x, _y}) 
+            || Intersects({_x + _width, _y}) 
+            || Intersects({_x, _y + _height}) 
+            || Intersects({_x + _width, _y + _height});
     }
 
     virtual void Insert(GlyphPtr, int) {}
     virtual void Add(GlyphPtr glyph) {}
 
     void SetPosition(const Point& p) {
-        params.x = p.x;
-        params.y = p.y;
+        x = p.x;
+        y = p.y;
     }
 
     void SetPosition(int x, int y) {
-        params.x = x;
-        params.y = y;
+        x = x;
+        y = y;
     }
 
     virtual void MoveGlyph(int x, int y) {
-        SetPosition(params.x + x, params.y + y);
+        SetPosition(x + x, y + y);
     }
 
-    void SetGlyphParams(const GlyphParams& _params) { params = _params; }
-    const GlyphParams& GetGlyphParams() const { return params; }
+    void SetGlyphParams(const int _x, const int _y, const int _width, const int _height) { 
+        x = _x;
+        y = _y;
+        width = _width;
+        height = _height;
+    }
 
-    int GetWidth() const { return params.width; }
-    int GetHeight() const { return params.height; }
+    int GetWidth() const { return width; }
+    int GetHeight() const { return height; }
 
-    Point GetPosition() const { return { params.x, params.y }; }
+    Point GetPosition() const { return { x, y }; }
 
-    int GetBottomBorder() const noexcept { return params.y + params.height; }
-    int GetRightBorder() const noexcept { return params.x + params.width; }
+    int GetBottomBorder() const noexcept { return y + height; }
+    int GetRightBorder() const noexcept { return x + width; }
+
+    friend std::ostream& operator<<(std::ostream& os, const Glyph& glyph) {
+        os << "x: " << glyph.x << " y: " << glyph.y << " width: " << glyph.width << " height: " << glyph.height;
+        return os;
+    }
 
 protected:
     // glyph is set by the coordinates of the upper-left corner, width and height
-    GlyphParams params;
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
 };
 
 #endif  // TEXT_EDITOR_GLYPH
