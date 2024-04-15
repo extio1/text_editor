98d8118ba76247bd155905c84e461a40a8b95e6d
extio1 <shalnevtimofey@gmail.com>
2024-04-15 10:41:33 +0700

    [UP-10] First several interfaces
    Not final project structure

---
+++ src/types.h
@@ -0,0 +1,16 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+class Rectangle{
+
+};
+
+class Circle{
+
+};
+
+class Point{
+
+};
+
+#endif /* GEOMETRY_H */
---
+++ src/window.h
@@ -0,0 +1,11 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+class Window{
+public:
+
+private:
+
+};
+
+#endif /* WINDOW_H */
