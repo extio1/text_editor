713402ade669936c0b91d90a33a19b4123c917e8
tatyanakrivonogova <tatyanakrivonogova@github.com>
2024-04-17 20:07:29 +0700

    Added secg for script

--- test/build_run.sh
+++ test/build_run.sh
@@ -140,7 +140,7 @@ if [[ -n $REPORT ]]; then
         # gcov will have already run, just grab info for lcov
         lcov --capture --directory . --output-file "$test".info > /dev/null
         pwd
-        genhtml "$test".info --output-directory ../../"$test"-html > /dev/null
+        genhtml "$test".info --output-directory ../../../"$test"-html > /dev/null
         cd ../../../.. || exit  # Back up to test dir
     done
 fi
