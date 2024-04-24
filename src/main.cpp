78317eab4e1db037d5cc59687715d91a7e562e39
tatyanakrivonogova <tatyanakrivonogova@github.com>
2024-04-24 22:21:36 +0700

    UP-10 Composition debug

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "../include/glyphs/character.h"
 #include "../include/glyphs/button.h"
 #include "../include/glyphs/monoglyph.h"
+#include "../include/glyphs/composition.h"
 
 int main() {
     std::cout << "Hello" << std::endl;
@@ -18,5 +19,16 @@ int main() {
     std::shared_ptr<Glyph> buttonPtr = std::make_shared<Button>(b);
     MonoGlyph m = MonoGlyph(buttonPtr);
     m.Draw();
+
+    Composition comp = Composition(3, 3, 3, 3);
+    comp.Add(std::make_shared<Character>(c));
+    comp.Insert(std::make_shared<Button>(b), 1);
+    std::cout << comp.GetGlyphPosition(std::make_shared<Character>(c)) << std::endl;
+    std::cout << comp.GetGlyphPosition(std::make_shared<Button>(b)) << std::endl;
+    comp.Draw();
+    comp.MoveGlyph(1, 2);
+    Glyph::GlyphPtr p = comp.Find(Point(4, 3));
+    std::cout << "Found glyph: " << std::endl;
+    p->Draw();
     return 0;
 }
