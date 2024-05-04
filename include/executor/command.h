dbaf2e09aac258f532ae7d16e085e123c703114f
extio1 <shalnevtimofey@gmail.com>
2024-05-04 21:44:19 +0700

    UP-17 Build system remake (more flexibility added). Executor MVP done
    Build: Now there is main build script in root directory defining
    main target (text_editor). Main script delegates building to script
    in src (test dir are going to be added in future). src build script delelates
    building concete module (executor here, but more modules e.g. window, glyph are going to be added).
    
    Executor: class Executor implemented. Encapsulates circular buffer of commands, have
    functions to Do, Redo and Undo commands from the circular buffer.

--- include/executor/command.h
+++ include/executor/command.h
@@ -1,19 +1,29 @@
 #ifndef TEXTEDITOR_INCLUDEEXECUTOR_COMMAND_H_
 #define TEXTEDITOR_INCLUDEEXECUTOR_COMMAND_H_
 
+#include <cstdio>
+
+/*
+ * Base class for commands that can be executed by Executor.
+ */
 class Command 
 {
 public:
-    Command() = default;
-    Command(Command&&) = default;
-    Command& operator=(Command&&) = default;
-
-    Command(const Command&) = delete;
-    Command& operator=(const Command&) = delete;
-
-    virtual void Execute(){};
-
+    virtual void Execute() = 0;
     virtual ~Command(){};
+};
+
+
+/*
+ * Commands that can be unexecuted by Executor.
+ * Not all commands are reversible, for instance,
+ * there no sense to reverse saving of document.
+ */
+class ReversibleCommand: public Command 
+{
+public:
+    virtual void Unexecute() = 0;
+    virtual ~ReversibleCommand(){};
 };
 
 #endif // TEXTEDITOR_INCLUDEEXECUTOR_COMMAND_H_
--- src/executor/executor.cpp
+++ src/executor/executor.cpp
@@ -1,17 +1,40 @@
 #include "executor/executor.h"
 
-#include <iostream>
-
 Executor::Executor(const std::size_t command_queue_length):
-    command_history(std::vector<Command>(command_queue_length)),
-    new_command_indx(0)
-    {}
+    command_history(CircularBuffer<std::shared_ptr<Command>>(command_queue_length)),
+    n_executed(0),
+    n_unexecuted(0)
+{}
 
-void Executor::Do(Command&& command)
+void Executor::Do(std::shared_ptr<Command>&& command)
 {
-    command_history[new_command_indx++] = std::move(command);
+    command->Execute();
+    command_history.push(std::move(command));
 
-    for (auto&& c: command_history){
-        printf("%p\n", &c);
+    ++n_executed;
+    n_unexecuted = 0;
+}
+
+void Executor::Redo()
+{
+    if( n_unexecuted > 0){
+        auto c = command_history.get_next();
+        c->Execute();
+
+        ++n_executed;
+        --n_unexecuted;
+    }
+}
+
+void Executor::Undo()
+{
+    if( n_executed > 0 ){
+        auto c = command_history.pop();
+        auto rc = std::dynamic_pointer_cast<ReversibleCommand>(c);
+        if(rc)
+            rc->Unexecute();
+
+        --n_executed;
+        ++n_unexecuted;
     }
 }
