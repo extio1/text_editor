58e44c4825868bcdcc9806d18f797c9dee60e690
extio1 <shalnevtimofey@gmail.com>
2024-05-26 13:35:27 +0700

    UP-20 Document class only saved via pointer to IDocument

--- src/executor/command/save_document.cpp
+++ src/executor/command/save_document.cpp
@@ -1,16 +1,17 @@
 #include "executor/command/save_document.h"
 
-#include <boost/archive/text_oarchive.hpp>
 #include <fstream>
-#include <utility>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 
-//SaveDocument::SaveDocument(Document& doc, std::string path)
-//    : doc(doc), path(std::move(path)) {}
-//
-//void SaveDocument::Execute() {
-//    std::ofstream ofs(path);
-//    boost::archive::text_oarchive oa(ofs);
-//    oa << doc;
-//}
-//
-//SaveDocument::~SaveDocument() {}
+SaveDocument::SaveDocument(IDocument& doc, std::string path):
+doc(doc), path(std::move(path)){}
+
+void SaveDocument::Execute()
+{
+    std::ofstream ofs(path);
+    boost::archive::text_oarchive oa(ofs);
+    oa << &doc;
+}
+
+SaveDocument::~SaveDocument() = default;
